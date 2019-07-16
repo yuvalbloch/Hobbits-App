3837c2890270cd1e1cdce7021d8382bf2c35f552
danielnaon <danielnaon@gmail.com>
2019-07-16 10:47:22 +0300

    its working

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// Server setup
 const express = require('express')
 const app = express()
-const api = require('./server/routes/api')
-const path = require('path')
-​
 const bodyParser = require('body-parser')
+const request = require('request')
+const path = require('path')
+const mongoose = require('mongoose')
+const api = require('./server/routes/api')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-​
-// Mongoose setup
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/HobbitsDB', { useNewUrlParser: true })
-​
-app.use('/', api)
+app.use('/', api);
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
-​
-​
-const port =3000
-app.listen(port, function () {
-    console.log(`Running on port ${port}`)
+mongoose.connect('mongodb://localhost/HobbitsDB', { useNewUrlParser: true })
+
+const port = 3000
+app.listen(port, function(){
+    console.log("server started on port: " + port)
 })
---
+++ server/routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const request = require('request')
+
+
+
+
+module.exports = router

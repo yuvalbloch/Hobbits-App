e5e207742a69f7d9a70226058ea3f9994295458f
danielnaon <danielnaon@gmail.com>
2019-07-16 15:35:04 +0300

    finally

--- dist/hobManager.js
+++ dist/hobManager.js
@@ -2,21 +2,21 @@ class hobManager{
     constructor() {
         this.user = {}
     }
-    saveData(data,userName ) {
+    saveData(data, userName ) {
         const status = {
             userName: userName,
             date: new Date(),
-            healtyFood: data.vegetabels + data.water,
-            sport: data.floor,
+            healtyFood: data.vegetables + data.water,
+            sport: data.floors,
             smiles: data.smiles,
         }
 
         this.user = status
         $.ajax({
-            type: "put",
-            url: "/updateuser/:" +userName ,
+            type: "PUT",
+            url:  `/updateuser/${userName}`,
             data: status,
-            success: function (data) {
+            success: function () {
                 console.log("sucses")
             },
         });

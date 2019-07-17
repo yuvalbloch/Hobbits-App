3becb33630ca44e1094ade38b48d44aecf5bfffd
u.v <yuval.bloch2@gmail.com>
2019-07-17 17:38:40 +0300

    st

--- dist/randerChatres.js
+++ dist/randerChatres.js
@@ -8,12 +8,12 @@ const paint= function (dates , numbers,canvas) {
                 label: '# of healthy lifestyle',
                 data: numbers,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255,1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
@@ -23,13 +23,16 @@ const paint= function (dates , numbers,canvas) {
                     'rgba(153, 102, 255, 1)',
                     'rgba(255, 159, 64, 1)'
                 ],
-                borderWidth: 1
+                borderWidth: 3
             }]
         },
         options: {
             scales: {
                 yAxes: [{
+
                     ticks: {
+                        fontColor: "black",
+                        fontSize : 17,
                         beginAtZero: true
                     }
                 }]

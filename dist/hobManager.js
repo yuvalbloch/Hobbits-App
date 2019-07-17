27543a6069d3ff52544ad2befa3e8997dc7b1577
u.v <yuval.bloch2@gmail.com>
2019-07-17 08:55:32 +0300

    not importent

--- dist/hobManager.js
+++ dist/hobManager.js
@@ -31,16 +31,16 @@ class hobManager{
         sort = sort.filter(u => u[0])
         sort = sort.map(u => u.filter(u =>u.date))
         sort = sort.map(u => u.filter(u=>u.date/86400000 +1 > now))
-        sort = sort.map(u => {return u[0]})
+        sort = sort.map(u => {return u[u.length-1]})
         let bestsInFood = sort.sort(function (a, b) {
             return b.healtyFood - a.healtyFood;
         }).slice(0,3).filter(m => m.userName);
         let bestsInSport = sort.sort(function (a, b) {
             return b.sport - a.sport;
-        }).slice(0,3).filter(m => m.userName);;
+        }).slice(0,3).filter(m => m.userName);
         let bestsInSmile = sort.sort(function (a, b) {
             return b.smile - a.smile;
-        }).slice(0,3).filter(m => m.userName);;
+        }).slice(0,3).filter(m => m.userName);
         bestsInSport =bestsInSport.map(m => {return m.userName})
         bestsInFood = bestsInFood.map(m => {return m.userName})
         bestsInSmile =bestsInSmile.map(m => {return m.userName})
